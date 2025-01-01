4716e1d64abf9877b61ee6e726193a03c686efc6
pujan chapagain <chapagain.pujan@gmail.com>
2025-01-01 14:59:41 +0545

    jay krishna:inital commit

---
+++ vite.config.js
@@ -0,0 +1,43 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import viteImagemin from 'vite-plugin-imagemin';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: resolve(__dirname, 'index.html'),
+    },
+    outDir: 'dist',
+    assetsDir: 'assets',
+    minify: 'esbuild',
+  },
+  plugins: [
+    viteImagemin({
+      gifsicle: {
+        optimizationLevel: 7,
+        interlaced: false,
+      },
+      optipng: {
+        optimizationLevel: 7,
+      },
+      mozjpeg: {
+        quality: 75,
+      },
+      svgo: {
+        plugins: [
+          {
+            name: 'removeViewBox',
+            active: false,
+          },
+          {
+            name: 'removeEmptyAttrs',
+            active: false,
+          },
+        ],
+      },
+      webp: {
+        quality: 75,
+      },
+    }),
+  ],
+});
